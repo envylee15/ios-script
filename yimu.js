ea0847670fae38eef4f328c2c0e8839a7b9fe7b7
envylee15 <118053262+envylee15@users.noreply.github.com>
2024-08-12 22:47:03 +0800

    Add files via upload

---
+++ yimu.js
@@ -0,0 +1,32 @@
+
+/*************************************
+
+项目名称：一木记账
+下载地址：https://t.cn/A6YRLuSR
+更新日期：2024-03-03
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/yimuapp\.com:8082\/bookkeeping\/user\/getUserInfoById url script-response-body yimu.js
+
+[mitm]
+hostname = yimuapp.com
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023.data = {
+  ...chxm1023.data,
+  "memberType" : "YEAR",
+  "memberExpiredTime" : 4092599349000,
+  "class" : "com.tian.OneWoodList.year.48"
+};
+chxm1023.data.openInfos.class = "com.tian.OneWoodList.year.48";
+
+$done({body : JSON.stringify(chxm1023)});
